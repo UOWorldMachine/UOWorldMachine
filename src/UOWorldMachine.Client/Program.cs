bfc4f24a2f81a497ff08e9e255fb13e9a873958b
3HMonkey <wewantthejoker@gmail.com>
2023-03-19 19:12:05 +0100

    Add project files.

---
+++ src/UOWorldMachine.Client/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace UOWorldMachine.Client
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
